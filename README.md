5c0b5a48f349539ac247ed42725153f410dbeb87
ChuramadiGyawali04 <churamadi.gyawali1234@gmail.com>
2023-06-16 14:26:38 +0545

    my boi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MERN
+<"hello word">
+dsjndhskdjskdj
---
+++ index.js
@@ -0,0 +1,35 @@
+// var a='123';
+// console.log(a);
+
+
+// let b='234';
+// console.log(b);
+
+// const c=232;
+// console.log(c);
+
+// // alert('HEY BEB');
+// // const age = prompt('body count?');
+// // console.log(age);
+
+// const isdon =confirm('are you don');
+// console.log(isdon);
+
+// let hour =11;
+// if(hour>10)
+
+let x=1;git config --global user.name "ChuramadiGyawali04"
+let y=2;
+if(x==1){
+    console.log("x is 1");
+}
+if (x == 1 && y == 2){
+    console.log('&& operator tested');
+}
+if ((x = 1)|| (y=3)) {
+    console.log('|| operator tested');
+}
+
+if (!x){
+    console.log('x is not ture')
+}
